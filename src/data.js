8379199e17e2d61b9d733a9e400ec11c6996daa5
s14ati <swati14012000@gmail.com>
2025-08-03 20:03:32 +0530

    Initial commit

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export const API_KEY = "AIzaSyDifIxBAOoh3MZPNVY_gyZKq27rswtnhnc";
+export const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
 
 export const value_converter = (value) =>{
     if (value >= 1000000) {
